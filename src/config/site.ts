2164e361284048c762f5f2a2ac1ce279ba2ff448
Shafnaa <saujanashafi2003@gmail.com>
2024-10-19 02:35:35 +0700

    chore: setting up the project

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+    name: "Meal Apps",
+    url: "https://meal-apps.saujanashafi.me",
+    ogImage: "https://ui.shadcn.com/og.jpg",
+    description:
+      "Meal Apps Demo for cmlabs Frontend Developer Internship Test.",
+    links: {
+      twitter: "https://twitter.com/saujanashafi",
+      github: "https://github.com/Shafnaa",
+    },
+  }
+  
+  export type SiteConfig = typeof siteConfig
